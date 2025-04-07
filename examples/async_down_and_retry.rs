cb75684f40adf908fec9fa988c29f3e33c9353aa
ixmoyren <mxonren@foxmail.com>
2025-04-07 22:41:14 +0800

    调整一下样例代码，减少 clone

--- examples/async_down_and_retry.rs
+++ examples/async_down_and_retry.rs
@@ -10,21 +10,23 @@ async fn main() {
         .connect_timeout(Duration::from_secs(10))
         .build()
         .unwrap();
-    let dir = tempfile::tempdir().unwrap();
-    let url = Url::from_str("https://www.7-zip.org/a/7z2409-linux-x64.tar.xz").unwrap();
+    // 需要下载的文件
     let filename = "7z2409-linux-x64.tar.gz";
-    let file_path = dir.path().join(filename);
-    let param = DownloadParam::try_new(url, filename, dir.into_path()).unwrap();
+    // 创建一个临时文件夹用来保存文件
+    let dir = tempfile::tempdir().unwrap();
+    let dir_path = dir.path();
     // 重试策略，使用 Fibonacci，并且重试 5 次
     let fibonacci_backoff = FibonacciBackoff::from_millis(1000).take(5);
     let summary = retry::spawn(fibonacci_backoff, async || {
-        download::spawn(client.clone(), param.clone()).await
+        let url = Url::from_str("https://www.7-zip.org/a/7z2409-linux-x64.tar.xz").unwrap();
+        let param = DownloadParam::try_new(url, filename, dir_path).unwrap();
+        download::spawn(client.clone(), param).await
     })
     .await
     .unwrap();
     let file = tokio::fs::OpenOptions::new()
         .read(true)
-        .open(&file_path)
+        .open(dir_path.join(filename))
         .await
         .unwrap();
     let file_len = file.metadata().await.unwrap().len();
