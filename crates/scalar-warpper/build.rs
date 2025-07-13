8f7b9a1b810f8cea4debf106290a8ee5ac4c5f3c
ixmoyren <mxonren@foxmail.com>
2025-07-14 00:17:15 +0800

    使用 npm 上 @scalar/api-reference 代替原本的静态文件，可以更好管理 @scalar 的版本

---
+++ crates/scalar-warpper/build.rs
@@ -0,0 +1,36 @@
+use std::process::{Command, Output};
+
+fn main() {
+    // 监听可能变化的文件，当文件变化则重新构建
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=package.json");
+    println!("cargo:rerun-if-changed=pnpm-lock.yaml");
+    println!("cargo:rerun-if-changed=pnpm-workspace.yaml");
+    // 更新前端依赖
+    let output = Command::new("pnpm")
+        .arg("update")
+        .output()
+        .expect("Failed to execute pnpm update");
+    print_output(output);
+    // 安装前端依赖
+    let output = Command::new("pnpm")
+        .arg("install")
+        .output()
+        .expect("Failed to execute pnpm install");
+    print_output(output);
+    // 压缩 scala-api-reference.js
+    let output = Command::new("pnpm")
+        .arg("run")
+        .arg("build:compress")
+        .output()
+        .expect("Failed to execute pnpm run build:compress");
+    print_output(output);
+}
+
+fn print_output(output: Output) {
+    if output.status.success() {
+        println!("cargo:warning={}", String::from_utf8_lossy(&output.stdout));
+    } else {
+        println!("cargo:error={}", String::from_utf8_lossy(&output.stderr));
+    }
+}
