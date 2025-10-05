27f1d598f9a501b7a0df9779d96ecae73e417f8c
ixmoyren <mxonren@foxmail.com>
2025-10-05 16:47:02 +0800

    使用 unwrap 来提前终止 build.rs 的执行，在这里使用 anyhow::Result 的作用不是很大

--- crates/llama-cpp-sys/build.rs
+++ crates/llama-cpp-sys/build.rs
@@ -1,4 +1,3 @@
-use anyhow::{Context, bail};
 use bindgen::{Bindings, RustEdition};
 use cmake::Config;
 use glob::glob;
@@ -53,30 +52,33 @@ impl From<&str> for CMakeBuildType {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    // 定义 bindgen 生成代码文件的目录
-    let binding_rs_out_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?).join("llama");
+fn main() {
+    let cargo_manifest_dir = PathBuf::from(
+        env::var("CARGO_MANIFEST_DIR").expect("Couldn't get cargo manifest dir from env"),
+    );
+    // 将 bindgen 生成代码文件放置到该目录下
+    let binding_rs_out_dir = cargo_manifest_dir.join("llama");
     // 编译产物所在的路径
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Couldn't get out dir form env"));
     // llama.cpp 源码路径
-    let llama_src_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?).join("llama.cpp");
+    let llama_src_dir = cargo_manifest_dir.join("llama.cpp");
 
     // 监听可能变化的文件，当文件变化则重新构建
-    cargo_rerun_if_file_changed(&llama_src_dir)?;
+    cargo_rerun_if_file_changed(&llama_src_dir);
 
     // bindgen 配置
-    let bindings = make_bindgen(&llama_src_dir)?;
+    let bindings = make_bindgen(&llama_src_dir);
     // 指定生成的代码写入的文件，显式指定这个路径，目的为了 IDE 或者 LSP 可以分析到这个文件，进而提供更好的提示和代码完成
     bindings
         .write_to_file(binding_rs_out_dir.join("bindings.rs"))
-        .context("Failed to write bindings")?;
+        .expect("Failed to change binding's config when set out file");
 
     // 获取目标三元组，针对不同的操作系统做不同的配置
-    let target = TargetTriple::parse_from_env()?;
+    let target = TargetTriple::parse_from_env().expect("Failed to get target triple from env");
     println!("cargo:warning={target:?}");
 
     // Cmake 配置，详情可以通过 llama.cpp 的 CMakeLists.txt 中了解
-    let mut cmake_config = make_cmake_config(&llama_src_dir, &target)?;
+    let mut cmake_config = make_cmake_config(&llama_src_dir, &target);
 
     // 如果是苹果的系统，那么不编译 GGML_BLAS
     if target.is_apple() {
@@ -86,9 +88,8 @@ fn main() -> anyhow::Result<()> {
     // 安卓系统配置
     if target.is_android() {
         // 需要通过环境变量获取到 NDK 的所在目录
-        let android_ndk = env::var("ANDROID_NDK").context(
-            "Please install Android NDK and ensure that ANDROID_NDK env variable is set",
-        )?;
+        let android_ndk = env::var("ANDROID_NDK")
+            .expect("Please install Android NDK and ensure that ANDROID_NDK env variable is set");
         println!("cargo::rerun-if-env-changed=ANDROID_NDK");
 
         // 指明 NDK 工具链
@@ -113,7 +114,8 @@ fn main() -> anyhow::Result<()> {
             cmake_config.cflag("-march=i686");
             cmake_config.cxxflag("-march=i686");
         } else {
-            bail!("Unsupported Android target {target:?}")
+            println!("cargo:error=Unsupported Android target {target:?}");
+            return;
         }
 
         // 不将 LLAMA.CPP 打包成一个单一文件
@@ -129,45 +131,42 @@ fn main() -> anyhow::Result<()> {
     // 针对 feature 进行配置
     // 开启 vulkan，需要开启 cmake 的 vulkan 功能标志，并提供 vulkan 的 lib，供 rustc 链接
     #[cfg(feature = "vulkan")]
-    open_vulkan_backend(&mut cmake_config, &target)?;
+    open_vulkan_backend(&mut cmake_config, &target);
 
     // 开启 cuda 功能则需要开启 cmake 的 cuda 功能标志
     #[cfg(any(feature = "cuda", feature = "cuda-no-vmm"))]
-    open_cuda_backend(&mut cmake_config)?;
+    open_cuda_backend(&mut cmake_config);
 
     // 开启 openmp 功能（OpenMP主要用于多线程并行计算）
     // openmp 在安卓上性能提升不明显，安卓平台优先使用 vulkan 编译
     #[cfg(feature = "openmp")]
-    open_openmp_backend(&mut cmake_config, &target)?;
+    open_openmp_backend(&mut cmake_config, &target);
 
     let build_dir = cmake_config.build();
     // 链接阶段，提供需要链接的 lib 目录
-    cargo_rustc_link_llama_cpp_lib(&out_dir, &build_dir, &target)?;
+    cargo_rustc_link_llama_cpp_lib(&out_dir, &build_dir, &target);
 
     // 链接 CPP 标准库
-    cargo_rustc_link_cpp_lib(&target)?;
+    cargo_rustc_link_cpp_lib(&target);
 
     #[cfg(any(feature = "cuda", feature = "cuda-no-vmm"))]
-    cargo_rustc_link_cuda_lib(&target)?;
+    cargo_rustc_link_cuda_lib(&target);
 
     #[cfg(feature = "openmp")]
-    cargo_rustc_link_openmp_lib(&target)?;
-
-    Ok(())
+    cargo_rustc_link_openmp_lib(&target);
 }
 
 /// 设置 rustc 链接到 openmp 的动态库
 #[cfg(feature = "openmp")]
-fn cargo_rustc_link_openmp_lib(target: &TargetTriple) -> anyhow::Result<()> {
+fn cargo_rustc_link_openmp_lib(target: &TargetTriple) {
     if target.is_gnu() {
         println!("cargo:rustc-link-lib=gomp");
     }
-    Ok(())
 }
 
 /// 设置 rustc 链接到 CUDA 的动态库
 #[cfg(any(feature = "cuda", feature = "cuda-no-vmm"))]
-fn cargo_rustc_link_cuda_lib(target: &TargetTriple) -> anyhow::Result<()> {
+fn cargo_rustc_link_cuda_lib(target: &TargetTriple) {
     // 寻找 cuda 安装的路径
     for lib_dir in find_cuda_helper::find_cuda_lib_dirs() {
         println!("cargo:rustc-link-search=native={}", lib_dir.display());
@@ -188,11 +187,10 @@ fn cargo_rustc_link_cuda_lib(target: &TargetTriple) -> anyhow::Result<()> {
     }
 
     println!("cargo:rustc-link-lib=static=culibos");
-    Ok(())
 }
 
 /// 设置 rustc 链接 C++ 标准库
-fn cargo_rustc_link_cpp_lib(target: &TargetTriple) -> anyhow::Result<()> {
+fn cargo_rustc_link_cpp_lib(target: &TargetTriple) {
     // 链接 c++ runtime
     if target.is_linux() {
         println!("cargo:rustc-link-lib=dylib=stdc++");
@@ -204,69 +202,62 @@ fn cargo_rustc_link_cpp_lib(target: &TargetTriple) -> anyhow::Result<()> {
         println!("cargo:rustc-link-lib=c++");
 
         if target.is_apple_darwin() {
-            let path = macos_link_search_path()?;
+            let path = macos_link_search_path();
             println!("cargo:rustc-link-lib=clang_rt.osx");
             println!("cargo:rustc-link-search={path}");
         }
     }
-
-    Ok(())
 }
 
-fn macos_link_search_path() -> anyhow::Result<String> {
-    let output = Command::new("clang").arg("--print-search-dirs").output()?;
+fn macos_link_search_path() -> String {
+    let output = Command::new("clang")
+        .arg("--print-search-dirs")
+        .output()
+        .expect("Failed to run 'clang --print-search-dirs'");
     if !output.status.success() {
-        bail!("failed to run 'clang --print-search-dirs', continuing without a link search path")
+        panic!("failed to run 'clang --print-search-dirs', continuing without a link search path")
     }
 
     let stdout = String::from_utf8_lossy(&output.stdout);
     for line in stdout.lines() {
         if line.contains("libraries: =") {
             let path = line.split('=').nth(1).unwrap();
-            return Ok(format!("{path}/lib/darwin"));
+            return format!("{path}/lib/darwin");
         }
     }
 
-    bail!("failed to determine the link search path, continuing without it")
+    panic!("failed to determine the link search path, continuing without it")
 }
 
 /// 设置 rustc 链接到 LLAMA.CPP 的编译产物
-fn cargo_rustc_link_llama_cpp_lib(
-    out: &Path,
-    build: &Path,
-    target: &TargetTriple,
-) -> anyhow::Result<()> {
+fn cargo_rustc_link_llama_cpp_lib(out: &Path, build: &Path, target: &TargetTriple) {
     println!("cargo:rustc-link-search={}", out.join("lib").display());
     println!("cargo:rustc-link-search={}", out.join("lib64").display());
     println!("cargo:rustc-link-search={}", build.display());
     let llama_libs_kind = "static";
-    let llama_libs = extract_lib_names(out, target)?;
+    let llama_libs = extract_lib_names(out, target);
     assert_ne!(llama_libs.len(), 0);
 
     for lib in llama_libs {
         // 静态链接到这些编译后的库产物中
-        println!("cargo:rustc-link-lib={llama_libs_kind}={lib}",);
+        println!("cargo:rustc-link-lib={llama_libs_kind}={lib}");
     }
-
-    Ok(())
 }
 
 /// 调整 lib 的名称
-fn extract_lib_names(out_dir: &Path, target: &TargetTriple) -> anyhow::Result<Vec<String>> {
+fn extract_lib_names(out_dir: &Path, target: &TargetTriple) -> Vec<String> {
     let lib_pattern = if target.is_windows() { "*.lib" } else { "*.a" };
     let libs_dir = out_dir.join("lib*");
     let pattern = libs_dir.join(lib_pattern);
     println!("cargo:warning=Extract libs {}", pattern.display());
 
-    let Some(pattern) = pattern.to_str() else {
-        bail!("Failed to get lib pattern str")
-    };
+    let pattern = pattern.to_str().expect("Failed to get lib pattern str");
 
     let mut lib_names: Vec<String> = Vec::new();
     // 文件的后缀名为 a
     let path_extension_a = std::ffi::OsStr::new("a");
     // 通过指定的 pattern 找到所有编译生成的库产物
-    for entry in glob(pattern)? {
+    for entry in glob(pattern).unwrap() {
         match entry {
             Ok(path) => {
                 if let Some(stem) = path.file_stem()
@@ -276,58 +267,56 @@ fn extract_lib_names(out_dir: &Path, target: &TargetTriple) -> anyhow::Result<Ve
                     let lib_name = if stem_str.starts_with("lib") {
                         stem_str.strip_prefix("lib").unwrap_or(stem_str)
                     } else {
-                        let Some(extension) = path.extension() else {
-                            bail!("Failed to get lib file path extension, {path:?}")
-                        };
+                        let extension = path.extension().expect(
+                            format!("Failed to get lib file path extension, {path:?}").as_str(),
+                        );
                         if extension == path_extension_a
                             && let Some(parent) = path.parent()
                         {
                             let target = parent.join(format!("lib{stem_str}.a"));
-                            rename(&path, &target).context("Failed to rename lib")?;
+                            rename(&path, &target).expect("Failed to rename lib");
                         }
                         stem_str
                     };
                     lib_names.push(lib_name.to_string());
                 } else {
-                    bail!("Failed to get lib file stem str, {path:?}")
+                    panic!("Failed to get lib file stem str, {path:?}")
                 }
             }
-            Err(e) => bail!("Match failure, error was {e:?}"),
+            Err(e) => panic!("Match failure, error was {e:?}"),
         }
     }
-    Ok(lib_names)
+    lib_names
 }
 
 /// 针对 openmp 进行配置
 #[cfg(feature = "openmp")]
-fn open_openmp_backend(cmake_config: &mut Config, target: &TargetTriple) -> anyhow::Result<()> {
+fn open_openmp_backend(cmake_config: &mut Config, target: &TargetTriple) {
     // openmp 在安卓上性能提升不明显，安卓平台优先使用 vulkan 编译
     if !target.is_android() {
         cmake_config.define("GGML_OPENMP", "ON");
     } else {
         cmake_config.define("GGML_OPENMP", "OFF");
     }
-    Ok(())
 }
 
 /// 针对 CUDA 进行配置
 #[cfg(any(feature = "cuda", feature = "cuda-no-vmm"))]
-fn open_cuda_backend(cmake_config: &mut Config) -> anyhow::Result<()> {
+fn open_cuda_backend(cmake_config: &mut Config) {
     cmake_config.define("GGML_CUDA", "ON");
     if cfg!(feature = "cuda-no-vmm") {
         cmake_config.define("GGML_CUDA_NO_VMM", "ON");
     }
-    Ok(())
 }
 
 /// 针对 vulkan 进行配置
 #[cfg(feature = "vulkan")]
-fn open_vulkan_backend(cmake_config: &mut Config, target: &TargetTriple) -> anyhow::Result<()> {
+fn open_vulkan_backend(cmake_config: &mut Config, target: &TargetTriple) {
     cmake_config.define("GGML_VULKAN", "ON");
     if target.is_windows() {
         // 需要手动提供 vulkan 安装的目录
         let vulkan_path = env::var("VULKAN_SDK")
-            .context("Please install Vulkan SDK and ensure that VULKAN_SDK env variable is set")?;
+            .expect("Please install Vulkan SDK and ensure that VULKAN_SDK env variable is set");
         let vulkan_lib_path = Path::new(&vulkan_path).join("Lib");
         println!("cargo:rustc-link-search={}", vulkan_lib_path.display());
         println!("cargo:rustc-link-lib=vulkan-1");
@@ -340,7 +329,6 @@ fn open_vulkan_backend(cmake_config: &mut Config, target: &TargetTriple) -> anyh
     } else if target.is_linux() {
         println!("cargo:rustc-link-lib=vulkan");
     }
-    Ok(())
 }
 
 /// 构建 cmake 的配置
@@ -366,14 +354,14 @@ fn open_vulkan_backend(cmake_config: &mut Config, target: &TargetTriple) -> anyh
 /// 允许通过环境变量配置 LLAMA.CPP 在编译时是否使用静态运行时库，默认不使用
 ///
 /// 如果编译工具是 Windows 系统 msvc ，并且 CMake 的 profile 不是 Debug，手动添加优化标识
-fn make_cmake_config(llama_src: &Path, target: &TargetTriple) -> anyhow::Result<Config> {
+fn make_cmake_config(llama_src: &Path, target: &TargetTriple) -> Config {
     let mut cmake_config = Config::new(llama_src);
 
     // 允许通过环境变量设置 CMake 在构建项目时的并行级别
     let parallel_level = env::var("BUILD_PARALLEL_LEVEL")
         .map_or(
             std::thread::available_parallelism()
-                .context("Failed to obtain an estimate of the default amount of parallelism a program should use!")?
+                .expect("Failed to obtain an estimate of the default amount of parallelism a program should use!")
                 .get(), |v| v.parse::<usize>().expect("Please provide a number!"),
         );
     unsafe {
@@ -421,7 +409,7 @@ fn make_cmake_config(llama_src: &Path, target: &TargetTriple) -> anyhow::Result<
         }
     }
 
-    Ok(cmake_config)
+    cmake_config
 }
 
 /// 构建 Binding
@@ -433,8 +421,8 @@ fn make_cmake_config(llama_src: &Path, target: &TargetTriple) -> anyhow::Result<
 /// 指定生成的代码中的结构体派生 PartialEq
 ///
 /// 指定需要关注的函数和类型
-fn make_bindgen(llama_src: &Path) -> anyhow::Result<Bindings> {
-    let bindings = bindgen::Builder::default()
+fn make_bindgen(llama_src: &Path) -> Bindings {
+    bindgen::Builder::default()
         // 指定生成 2024 版本的代码
         .rust_edition(RustEdition::Edition2024)
         .header("wrapper.h")
@@ -450,12 +438,11 @@ fn make_bindgen(llama_src: &Path) -> anyhow::Result<Bindings> {
         // 不把 enum 附加到常量和 newType 变体
         .prepend_enum_name(false)
         .generate()
-        .context("Failed to generate bindings")?;
-    Ok(bindings)
+        .expect("Failed to generate bindings")
 }
 
 /// 监听文件，如果有变化，则重新运行 cargo
-fn cargo_rerun_if_file_changed(llama_src: &Path) -> anyhow::Result<()> {
+fn cargo_rerun_if_file_changed(llama_src: &Path) {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=wrapper.h");
     // 不监听一整个 llama.cpp 文件夹，这样会触发一些不必要的构建
@@ -469,7 +456,7 @@ fn cargo_rerun_if_file_changed(llama_src: &Path) -> anyhow::Result<()> {
                 .unwrap_or_default()
         });
     for entry in entry_iter {
-        let entry = entry.context("Failed to obtain file entry!")?;
+        let entry = entry.expect("Failed to obtain file entry when watching llama.cpp!");
         // 判断当前文件的名称是否包含 CMakeLists.txt，包含则需要监听
         let contain_cmake = entry
             .file_name()
@@ -484,6 +471,4 @@ fn cargo_rerun_if_file_changed(llama_src: &Path) -> anyhow::Result<()> {
             println!("cargo:rerun-if-changed={}", entry.path().display());
         }
     }
-
-    Ok(())
 }
